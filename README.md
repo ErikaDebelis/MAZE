de92c732dfc03d4018203ca5800e28b4affdd543
Erika Debelis <erika.debelis@gmail.com>
2021-09-24 09:00:26 -0700

    begin creating html image maps for buttons to next door/page

--- README.md
+++ README.md
@@ -7,4 +7,8 @@
 * 12:00: lunch
 * 1:40: research into react w asp.net core https://docs.microsoft.com/en-us/aspnet/core/client-side/spa/react?view=aspnetcore-5.0&tabs=visual-studio
 * 2:30: trying to fix verisoning/ build issues with babel-eslint
-4:00: researching how to build/ configure react w asp.net core https://www.pluralsight.com/courses/aspdotnet-core-react-building-website?aid=701j0000001heIoAAI&promo=&utm_source=non_branded&utm_medium=digital_paid_search_google&utm_campaign=US_Dynamic&utm_content=&cq_cmp=175953558&gclid=CjwKCAjw-ZCKBhBkEiwAM4qfF1e0YtBlyLFSWNRJL4b5vUk5hOwt6128rJLNE2u7xxZg5C-YdRctURoCTNEQAvD_BwE  https://www.youtube.com/watch?v=gpfP60KjmZU
+* 4:00: researching how to build/ configure react w asp.net core https://www.pluralsight.com/courses/aspdotnet-core-react-building-website?aid=701j0000001heIoAAI&promo=&utm_source=non_branded&utm_medium=digital_paid_search_google&utm_campaign=US_Dynamic&utm_content=&cq_cmp=175953558&gclid=CjwKCAjw-ZCKBhBkEiwAM4qfF1e0YtBlyLFSWNRJL4b5vUk5hOwt6128rJLNE2u7xxZg5C-YdRctURoCTNEQAvD_BwE  https://www.youtube.com/watch?v=gpfP60KjmZU
+
+
+
+
---
+++ client/src/components/html-image-map.js
@@ -0,0 +1,163 @@
+<img src="1.PNG" usemap="#pg1-image-map">
+<map name="pg1-image-map">
+    <area
+      target=""
+      alt="door 20"
+      title="20"
+      href="20.PNG"
+      onclick = "show('20.PNG')
+      coords="223,84,299,117"
+      shape="rect">
+
+    <area
+      target=""
+      alt="door 26"
+      title="26"
+      href="26.PNG"
+      onclick = "show('26.PNG')
+      coords="431,81,510,117"
+      shape="rect">
+
+    <area
+      target=""
+      alt="door 41"
+      title="41"
+      href="41.PNG"
+      onclick = "show('41.PNG')
+      coords="586,115,649,113,641,71,585,78"
+      shape="poly">
+
+    <area
+      target=""
+      alt="door 21"
+      title="21"
+      href="21.PNG"
+      onclick = "show('21.PNG')
+      coords="698,61,701,110,772,105,767,55"
+      shape="poly">
+</map>
+
+
+
+
+<img src="2.PNG" usemap="#pg2-image-map">
+<map name="pg2-image-map">
+    <area
+      target=""
+      alt="door 29"
+      title="29"
+      href="29.PNG"
+      onclick = "show('29.PNG')
+      coords="89,80,91,184,156,184,156,93"
+      shape="poly">
+
+    <area
+      target=""
+      alt="door 22"
+      title="22"
+      href="22.PNG"
+      onclick = "show('22.PNG')
+      coords="359,111,358,177,461,178,460,112"
+      shape="poly">
+
+    <area
+      target=""
+      alt="door 12"
+      title="12"
+      href="12.PNG"
+      onclick = "show('12.PNG')
+      coords="654,82,668,172,726,173,711,73"
+      shape="poly">
+</map>
+
+
+
+
+<img src="3.PNG" usemap="#pg3-image-map">
+<map name="pg3-image-map">
+    <area
+      target=""
+      alt="door 9"
+      title="9"
+      href="9.PNG"
+      onclick = "show('9.PNG')
+      coords="446,64,549,117"
+      shape="rect">
+
+    <area
+      target=""
+      alt="door 18"
+      title="18"
+      href="18.PNG"
+      onclick = "show('18.PNG')
+      coords="671,45,666,112,750,107,755,25"
+      shape="poly">
+</map>
+
+
+
+<img src="4.PNG" usemap="#pg4-image-map">
+<map name="pg4-image-map">
+    <area
+      target=""
+      alt="door 44"
+      title="44"
+      href="44.PNG"
+      onclick = "show('44.PNG')
+      coords="74,41,53,115,163,137,179,82"
+      shape="poly">
+
+    <area
+      target=""
+      alt="door 29"
+      title="29"
+      href="29.PNG"
+      onclick = "show('29.PNG')
+      coords="231,100,218,148,264,159,277,118"
+      shape="poly">
+
+    <area
+      target=""
+      alt="door 15"
+      title="15"
+      href="15.PNG"
+      onclick = "show('15.PNG')
+      coords="300,125,288,164,312,168,322,136"
+      shape="poly">
+
+    <area
+      target=""
+      alt="door 11"
+      title="11"
+      href="11.PNG"
+      onclick = "show('11.PNG')
+      coords="371,148,442,183"
+      shape="rect">
+
+    <area
+      target=""
+      alt="door 16"
+      title="16"
+      href="16.PNG"
+      onclick = "show('16.PNG')
+      coords="492,135,502,170,527,164,515,125"
+      shape="poly">
+
+    <area
+      target=""
+      alt="door 24"
+      title="24"
+      href="24.PNG"
+      onclick = "show('24.PNG')
+      coords="540,118,553,161,598,153,583,102"
+      shape="poly">
+
+    <area
+      target=""
+      alt="door 43"
+      title="43"
+      href="43.PNG"
+      onclick = "show('43.PNG')
+      coords="634,81,655,142,766,122,746,42"
+      shape="poly">
+</map>
