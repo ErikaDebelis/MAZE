2c27b93b5f8368771b4dc7719577ce9d5f93f5be
Erika Debelis <erika.debelis@gmail.com>
2021-10-07 14:46:45 -0700

    finishing touches

--- client/src/components/directions.js
+++ client/src/components/directions.js
@@ -6,6 +6,7 @@ export class Directions extends Component {
   render() {
     return (
       <div>
+        <p className="textlight">click the door numbers on the page to change the page and move through the doors!</p>
         <img src="/images/Directions.PNG" alt="directions page"></img>
       </div>
     );
