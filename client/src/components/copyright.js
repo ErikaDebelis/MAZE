f16506a0aa0392d1034042950ce378ea29a7bc31
Erika Debelis <erika.debelis@gmail.com>
2021-10-07 12:04:32 -0700

    add creepy font styling

--- client/src/components/copyright.js
+++ client/src/components/copyright.js
@@ -11,14 +11,13 @@ export class Copyright extends Component {
         <br/>
         <br/>
         <h3>Website Disclaimer</h3>
-        <p>The information used by Erika Debelis on https://github.com/ErikaDebelis/MAZE (the "Site") is for educational purposes only. All information on the Site is used for demonstration purposes only and I (Erika Debelis) claim no rights to the contents of the book MAZE by Christopher Manson. I only claim the rights and ownership of the code written to make the dynamic web application. If anyone enjoys this web application of MAZE please find and purchase MAZE, this a lovely book.</p>
+        <p>The information used by Erika Debelis on <a href="https://github.com/ErikaDebelis/MAZE">https://github.com/ErikaDebelis/MAZE</a> (the "Site") is for educational purposes only. All information on the Site is used for demonstration purposes only and I (Erika Debelis) claim no rights to the contents of the book MAZE by Christopher Manson. I only claim the rights and ownership of the code written to make the dynamic web application. If anyone enjoys this web application of MAZE please find and purchase MAZE, this a lovely book.</p>
         <br/>
         <br/>
         <h3>Fair Use</h3>
         <p>Copyright Disclaimer under section 107 of the Copyright Act of 1976, allowance is made for "fair use" for purposes such as criticism, comment, news reporting, teaching, scholarship, education and research.</p>
         <br/>
         <p>Fair use is a use permitted by copyright statute that might otherwise be infringing.</p>
-        <br/>
         <br/>
         <h3>Fair Use Definition</h3>
         <br/>
--- client/src/components/directions.js
+++ client/src/components/directions.js
@@ -10,10 +10,4 @@ export class Directions extends Component {
       </div>
     );
   }
-
-  async populateWeatherData() {
-    const response = await fetch('page');
-    const data = await response.json();
-    this.setState({ pages: data, loading: false });
-  }
 }
